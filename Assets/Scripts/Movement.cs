69e776db80544377c296b5113e00be21f743eff8
Lillian <ziqi.fan@ontariotechu.net>
2020-12-02 23:59:08 -0500

    Finished the demo

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.LowLevel;
+using System.IO.Ports;
 public class Movement : MonoBehaviour
 {
     public GameObject controllerLeft, controllerRight;
     Quaternion initRot;
     Vector2 LS = Vector2.zero, RS = Vector2.zero;
     float movementScale = 20, angle = 45;
+
+    SerialPort sp = new SerialPort("COM5", 9600);
+
+    public GameObject c1;
+    public GameObject c2;
+    public GameObject c3;
+    public GameObject c4;
 
     Controls control;
     void Awake()
@@ -21,10 +29,61 @@ public class Movement : MonoBehaviour
 
     }
 
+    void Start()
+    {
+        sp.Open();
+        sp.ReadTimeout = 100;
+    }
+
     void Update()
     {
         transform.position += Vector3.Scale((transform.forward * LS.y + transform.right * LS.x) * movementScale * Time.deltaTime, new Vector3(1, 0, 1));
         transform.rotation = transform.rotation * Quaternion.Euler(0, RS.x * angle *2* Time.deltaTime, 0);
+        
+        if (sp.IsOpen)
+        {
+            try
+            {
+                if(sp.ReadByte() == 97)
+                {
+                    c1.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+                }
+                else
+                {
+                    c1.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+                }
+                if (sp.ReadByte() == 98)
+                {
+                    c2.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+                }
+                else
+                {
+                    c2.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+                }
+                if (sp.ReadByte() == 99)
+                {
+                    c3.GetComponent<Renderer>().material.SetColor("_Color", Color.green);
+                }
+                else
+                {
+                    c3.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+                }
+                if (sp.ReadByte() == 100)
+                {
+                    c4.GetComponent<Renderer>().material.SetColor("_Color", Color.yellow);
+                }
+                else
+                {
+                    c4.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+                }
+                if (sp.ReadByte() <= 10)
+                {
+                    transform.Rotate(0, sp.ReadByte() - 5, 0, Space.Self);
+                }
+            }
+            catch (System.Exception) { 
+            }
+        }
     }
 
     void OnEnable()
